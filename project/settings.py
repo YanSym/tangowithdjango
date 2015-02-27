69b0f428dd5185b68e84e3ed44f4a9e176a9aa96
YanSym <yan.vsym1@gmail.com>
2015-02-27 19:05:44 +0000

    Chapter 12 - completed

--- project/settings.py
+++ project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'rango',
+	'registration', # add in the registration package
 )
 
 MIDDLEWARE_CLASSES = (
@@ -77,6 +78,13 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+REGISTRATION_OPEN = True        # If True, users can register
+ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
+REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
+LOGIN_REDIRECT_URL = '/rango/'  # The page you want users to arrive at after they successful log in
+LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
+                                                                # and are trying to access pages requiring authentication
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
